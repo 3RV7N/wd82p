263073479aacdf85a4c6e4677a9a0181a7b33333
Ervin Zapanta <ervinzapanta1@gmail.com>
2023-09-07 20:38:34 +0800

    Add index root

---
+++ 23-errors_and_clean_code/main.js
@@ -0,0 +1,18 @@
+// Errors and Clean Code
+// const a = "Hello";
+// const b = "World";
+
+// console.log(c);
+
+//Syntax Error
+// function printHelloWorld() {
+//  console.log "Hello, World!";
+// }
+
+// Type Error
+// -> an operand or argument passed to a function is not of the expected type by that operator of function.
+// -> or when attempting to modify a value that cannot be chaned;
+// -> or when attempting to use a value in an inappropriate way.
+
+const name = "Ervin";
+name = "Ervin";
