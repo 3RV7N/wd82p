9b7840d4088aebabf13511728b0d38ca0c14f194
Ervin Zapanta <ervinzapanta1@gmail.com>
2023-09-08 20:55:10 +0800

    Add index root

--- 23-errors_and_clean_code/main.js
+++ 23-errors_and_clean_code/main.js
@@ -14,5 +14,33 @@
 // -> or when attempting to modify a value that cannot be chaned;
 // -> or when attempting to use a value in an inappropriate way.
 
-const name = "Ervin";
-name = "Ervin";
+// const name = "Ervin";
+// name = "Ervin";
+
+// Clean Code
+
+// Naming functions and variables
+// -> meaningful
+// -> descriptive
+
+// Use a consistent vocabulary or naming convention
+
+const x = function(z) {
+  let w = 0;z.forEach(
+    function (q) {
+      w += q;
+    }
+  );return w;
+};
+
+x([2, 2, 2]);
+
+function sumArray(array) {
+  let sum = 0;
+
+  array.forEach((number) -> sum += number);
+
+  return sum;
+}
+
+sumArray ([2, 2, 2]);
---
+++ 24-dom_manipulation_and_events/main.js
@@ -0,0 +1,56 @@
+// DOM Manipulation & Events
+
+//Select nodes in the DOM
+
+//example
+//const container = document.querySelector("#container");
+
+//const content = document.createElement("div");
+//content.classList.add("content");
+//content.textContent = "This is a content text";
+
+//container.appendChild(content);
+
+//const display = document.querySelector(".display");
+
+//cREATE NODES IN THE DOM
+//const div = document.createElement("div");
+//div.classList.add("stop");
+
+//Append elements in the DOM or another node
+//container.appendChild(div);
+
+//Remove elements from the DOM or another node
+//container.removeChild(div);
+
+//const div2 = document.createElement("div");
+
+//div2.style.height = "100px";
+//div2.style.width = "100px";
+//div2.style.backgroundColor = "red";
+
+//
+
+//div2.setAttribute(
+//  "style",
+//  "height: 100px background-color: red; width: 100px;"
+//);
+
+//container.appendChild(div2);
+
+//div2.classList.add("red-box");
+//div2.classList.remove("red-box");
+//div2.classList.toggle("red-box");
+
+//div.textContent = "Hello World!";
+
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", changeText);
+
+function changeText() {
+  if (btn.textContent === "Clicked!") {
+    btn.textContent = "Click Me";
+  } else {
+    btn.textContent = "Clicked";
+  }
+}
