0a4e2506cb4f3b81b280879a5e9792da34a9fda7
Ervin Zapanta <ervinzapanta1@gmail.com>
2023-09-23 08:48:00 +0800

    Add index root

--- 33-jason_Javascript_object_notation/main.js
+++ 33-jason_Javascript_object_notation/main.js
@@ -1,41 +1,29 @@
-const shopItems = [
-  {
-    name: "Product 1",
-    price: 19.99,
-    description: "Lorem ipsum dolor sit amet concectetur adipiscing elit.",
-  },
-  {
-    name: "Product 2",
-    price: 29.99,
-    description: "Lorem ipsum dolor sit amet concectetur adipiscing elit.",
-  },
-  {
-    name: "Product 3",
-    price: 39.99,
-    description: "Lorem ipsum dolor sit amet concectetur adipiscing elit.",
-  },
-  {
-    name: "Product 4",
-    price: 39.99,
-    description: "Lorem ipsum dolor sit amet concectetur adipiscing elit.",
-  },
-];
+async function fetchShopItems(api) {
+  const response = await fetch(api);
+  const data = await response.json();
+
+  return data;
+}
 
 function renderShopItems(shopItems) {
   const shoppingList = document.getElementById("shopping-list");
 
   shopItems.forEach((shopItem) => {
-    const listItem = document.createElement("shopping-list");
+    const listItem = document.createElement("li");
     listItem.classList.add("shop-item");
 
     listItem.innerHTML = `
-      <h2>${shopItem.name}</h2>
-      <p>Price: ${shopItem.price}</p>
-      <p>${shopItem.description}</p>
-    `;
+          <img src=${shopItem.thumbnail} />
+          <h2>${shopItem.title}</h2>
+          <p>Price: ${shopItem.price}</p>
+          <p>${shopItem.description}</p>
+      `;
 
     shoppingList.appendChild(listItem);
   });
 }
 
-renderShopItems(shopItems);
+window.addEventListener("load", async () => {
+  const shopItems = await fetchShopItems("https://dummyjson.com/products");
+  renderShopItems(shopItems.products);
+});
